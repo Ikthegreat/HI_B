fe72b954f81256c29e50609cae0d2ee9ec2fdfd6
HBumzz <choi12920@gmail.com>
2023-05-22 08:29:55 +0900

    Fix(accounts)

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,6 @@ from . import views
 # Create your views here.
 urlpatterns = [
     path('profile/<username>/', views.profile_with_like_movies),
-    # path('profile/<username>/bookmark/', views.profile_with_bookmark_movies),
-    # path('profile/<username>/article/', views.profile_with_article),
     path('profile/<username>/comment/', views.profile_with_comment),
     path('profile/', views.get_my_profile),
     path('profile/<username>/follow/', views.follow),
